fb863479da0e34017a6ab86ea534c9c9686badcd
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-03-15 17:59:27 -0700

    bash...

--- .test.sh
+++ .test.sh
@@ -23,5 +23,5 @@ gometalinter \
 
 for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic "$d"
-    [ -f profile.out ] && cat profile.out >> coverage.txt && rm profile.out
+    if [ -f profile.out ]; then cat profile.out >> coverage.txt; rm -f profile.out; fi
 done
