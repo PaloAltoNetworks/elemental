9070a4106f0a1e97cc3cb1ace0fefcb95200eddc
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-05-01 11:17:54 -0700

    new: licensing

--- doc.go
+++ doc.go
@@ -1,3 +1,14 @@
+// Copyright 2019 Aporeto Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package elemental provides a set of interfaces and structures used to manage a model generated from a
 // Regolithe Specifications Set.
 //
@@ -22,4 +33,4 @@
 //
 // Elemental is mainly an abstract package and cannot really be used by itself. You must use the provided command
 // (elegen) to generate an Elemental Model from a Regolithe Specification Set.
-package elemental
+package elemental // import "go.aporeto.io/elemental"
