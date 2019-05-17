PaloAltoNetworks/elemental

commit 78b83781e14c63bce1860a9a02fa111f25b03221

encode: fix nil check (#40)
* fix: Encode should also check to see if the value in stored the interface is nil

Signed-off-by: Amir Aslaminejad <amir.a@aporeto.com>

* new: add test coverage to cover scenario where Encode is passed in an interface which holds nil values

Signed-off-by: Amir Aslaminejad <amir.a@aporeto.com>

.gitignore
@@ -11,3 +11,4 @@ cmd/elegen/build
 *.cov
 artifacts
 profile.out
+.idea/*

encoding.go
@@ -141,7 +141,7 @@ func Decode(encoding EncodingType, data []byte, dest interface{}) error {
 // from the given acceptType.
 func Encode(encoding EncodingType, obj interface{}) ([]byte, error) {
 
-	if obj == nil {
+	if obj == nil || reflect.ValueOf(obj).IsNil() {
 		return nil, fmt.Errorf("encode received a nil object")
 	}
 

encoding_test.go
@@ -110,6 +110,23 @@ func TestEncodeDecode(t *testing.T) {
 				So(err, ShouldNotBeNil)
 			})
 		})
+
+		Convey(fmt.Sprintf("Encode will throw an error if passed in a nil interface or an interface holding a nil value using encoding %s", encoding), t, func() {
+
+			var nilPointer *int
+			cases := map[string]interface{}{
+				"nil interface": nil,
+				"nil pointer":   nilPointer,
+			}
+
+			for description, object := range cases {
+				Convey(fmt.Sprintf("passing in a %s should result in an error", description), func() {
+					data, err := Encode(encoding, object)
+					So(data, ShouldBeNil)
+					So(err, ShouldNotBeNil)
+				})
+			}
+		})
 	}
 
 	test(EncodingTypeJSON)

commit f6748eac58e2dc61d2e0676901766121743ad6be

fix: addresses the bug introduced in #40 by actually checking if the type is a pointer

.gitignore
@@ -11,3 +11,4 @@ cmd/elegen/build
 *.cov
 artifacts
 profile.out
+.idea/*

encoding.go
@@ -141,8 +141,9 @@ func Decode(encoding EncodingType, data []byte, dest interface{}) error {
 // from the given acceptType.
 func Encode(encoding EncodingType, obj interface{}) ([]byte, error) {
 
-	if obj == nil {
-		return nil, fmt.Errorf("encode received a nil object")
+	valObj := reflect.ValueOf(obj)
+	if obj == nil || (valObj.Kind() == reflect.Ptr && valObj.IsNil()) {
+		return nil, fmt.Errorf("encode received a nil object or one containing a nil pointer")
 	}
 
 	var enc *codec.Encoder

encoding_test.go
@@ -110,6 +110,23 @@ func TestEncodeDecode(t *testing.T) {
 				So(err, ShouldNotBeNil)
 			})
 		})
+
+		Convey(fmt.Sprintf("Encode will throw an error if passed in a nil interface or an interface holding a nil pointer using encoding %s", encoding), t, func() {
+
+			var nilPointer *int
+			cases := map[string]interface{}{
+				"nil interface": nil,
+				"nil pointer":   nilPointer,
+			}
+
+			for description, object := range cases {
+				Convey(fmt.Sprintf("passing in a %s should result in an error", description), func() {
+					data, err := Encode(encoding, object)
+					So(data, ShouldBeNil)
+					So(err, ShouldNotBeNil)
+				})
+			}
+		})
 	}
 
 	test(EncodingTypeJSON)