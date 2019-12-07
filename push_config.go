fcdec5ab59501c84d1c5149d3e9e289b566830dd
Amir Aslaminejad <aaslamin@users.noreply.github.com>
2019-12-06 18:52:09 -0800

    Add support for fine-grained identity filtering on websockets (#61)
    * edit:
    
    - add new attribute 'IdentityFilters' to 'PushFilter'
    - rename 'PushFilter' to 'PushConfig' via type alias
    
    * edit:
    
    - make old constructor 'NewPushFilter' print to stdout a deprecation warning
    - add method 'ParseIdentityFilters' that parses the configured identity filters so they do not need to be re-parsed on each push event
    - eliminate remaining references to 'PushFilter'
    
    * edit: add test for old constructor - NewPushFilter
    
    * edit: update receiver variable naming for consistency
    
    new: add new method to 'PushConfig' - FilterForIdentity - which will allow consumer to fetch the "fine-grained" identity filter for a given identity
    
    * new: add unit tests for new method - ParseIdentityFilters
    
    * new: add unit tests for method - FilterForIdentity

---
+++ push_config.go
@@ -0,0 +1,198 @@
+// Copyright 2019 Aporeto Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package elemental
+
+import (
+	"fmt"
+	"net/url"
+)
+
+// A PushFilter represents an abstract filter for filtering out push notifications. This is now aliased to PushConfig as a
+// result of re-naming the type.
+//
+// Deprecated: use the new name PushConfig instead
+type PushFilter = PushConfig
+
+// A PushConfig represents an abstract filter for filtering out push notifications.
+//
+// The 'IdentityFilters' field is a mapping between a filtered identity and the string representation of an elemental.Filter.
+// A client will supply this attribute if they want fine-grained filtering on the set of identities that they are filtering on.
+// If this attribute has been supplied, the identities passed to 'IdentityFilters' must be a subset of the identities passed to
+// 'Identities'; passing in identities that are not provided in the 'Identities' field will be ignored.
+type PushConfig struct {
+	Identities      map[string][]EventType `msgpack:"identities" json:"identities"`
+	IdentityFilters map[string]string      `msgpack:"filters"    json:"filters"`
+	Params          url.Values             `msgpack:"parameters" json:"parameters"`
+
+	// parsedIdentityFilters holds the parsed `IdentityFilters` to avoid re-parsing the configured filters on each push
+	// event that is using the same config.
+	parsedIdentityFilters map[string]*Filter
+}
+
+// NewPushFilter returns a new PushFilter. NewPushFilter is now aliased to NewPushConfig. This was done for backwards
+// compatibility as a result of the re-naming of PushFilter to PushConfig.
+//
+// Deprecated: use the constructor with the new name, NewPushConfig, instead
+func NewPushFilter() *PushFilter {
+	fmt.Println("DEPRECATED: elemental.NewPushFilter is deprecated, use elemental.NewPushConfig instead")
+
+	return &PushFilter{
+		Identities:            map[string][]EventType{},
+		IdentityFilters:       map[string]string{},
+		parsedIdentityFilters: map[string]*Filter{},
+	}
+}
+
+// NewPushConfig returns a new PushConfig.
+func NewPushConfig() *PushConfig {
+
+	return &PushConfig{
+		Identities:            map[string][]EventType{},
+		IdentityFilters:       map[string]string{},
+		parsedIdentityFilters: map[string]*Filter{},
+	}
+}
+
+// SetParameter sets the values of the parameter with the given key.
+func (pc *PushConfig) SetParameter(key string, values ...string) {
+
+	if pc.Params == nil {
+		pc.Params = url.Values{}
+	}
+
+	pc.Params[key] = values
+}
+
+// Parameters returns a copy of all the parameters.
+func (pc *PushConfig) Parameters() url.Values {
+
+	if pc.Params == nil {
+		return nil
+	}
+
+	out := make(url.Values, len(pc.Params))
+	for k, v := range pc.Params {
+		out[k] = v
+	}
+
+	return out
+}
+
+// FilterIdentity adds the given identity for the given eventTypes in the PushConfig.
+func (pc *PushConfig) FilterIdentity(identityName string, eventTypes ...EventType) {
+
+	pc.Identities[identityName] = eventTypes
+}
+
+// ParseIdentityFilters parses the configured PushConfig's 'IdentityFilters' attribute to elemental filters.
+// The parsed filters will then be stored in the non-exposed 'parsedIdentityFilters' attribute of PushConfig. This is useful
+// for clients that wish the utilize the same filter multiple times without having to incur the overhead of parsing each time.
+//
+// An error is returned in following situations:
+//   - when a filter is declared on an identity that is not defined in the PushConfig's 'Identities' attribute
+//   - when a filter cannot be parsed into an elemental.Filter
+func (pc *PushConfig) ParseIdentityFilters() error {
+
+	if pc.parsedIdentityFilters == nil {
+		pc.parsedIdentityFilters = map[string]*Filter{}
+	}
+
+	for identity, unparsedFilter := range pc.IdentityFilters {
+		if _, found := pc.Identities[identity]; !found {
+			// in the event an error occurs we zero out the parsed identities to avoid having a partially set of parsed identities
+			pc.parsedIdentityFilters = map[string]*Filter{}
+			return fmt.Errorf("elemental: cannot declare an identity filter on %q as that was not declared in 'Identities'", identity)
+		}
+
+		filter, err := NewFilterParser(unparsedFilter).Parse()
+		if err != nil {
+			// in the event an error occurs we zero out the parsed identities to avoid having a partially set of parsed identities
+			pc.parsedIdentityFilters = map[string]*Filter{}
+			return fmt.Errorf("elemental: unable to parse filter %q: %s", unparsedFilter, err)
+		}
+
+		pc.parsedIdentityFilters[identity] = filter
+	}
+
+	return nil
+}
+
+// IsFilteredOut returns true if the given Identity is not part of the PushConfig's Identity mapping
+func (pc *PushConfig) IsFilteredOut(identityName string, eventType EventType) bool {
+
+	// if the identities list is empty, we filter nothing.
+	if len(pc.Identities) == 0 {
+		return false
+	}
+
+	// If it contains something, but not the identity, we filter out.
+	types, ok := pc.Identities[identityName]
+	if !ok {
+		return true
+	}
+
+	// If there is no event types defined we don't filter
+	if len(types) == 0 {
+		return false
+	}
+
+	// If if there are some event types defined, we don't filter out
+	// if the current event type is in the list.
+	for _, t := range types {
+		if t == eventType {
+			return false
+		}
+	}
+
+	return true
+}
+
+// FilterForIdentity returns the associated fine-grained filter for the given identity. In the event that no fine-grained
+// filter has been configured for the identity, the second return value (a boolean), will be set to false.
+func (pc *PushConfig) FilterForIdentity(identityName string) (*Filter, bool) {
+
+	if pc.parsedIdentityFilters == nil {
+		return nil, false
+	}
+
+	filter, found := pc.parsedIdentityFilters[identityName]
+	return filter, found
+}
+
+// Duplicate duplicates the PushConfig.
+func (pc *PushConfig) Duplicate() *PushConfig {
+
+	config := NewPushConfig()
+
+	for id, types := range pc.Identities {
+		config.FilterIdentity(id, types...)
+	}
+
+	for id, f := range pc.IdentityFilters {
+		config.IdentityFilters[id] = f
+	}
+
+	for id, f := range pc.parsedIdentityFilters {
+		config.parsedIdentityFilters[id] = f
+	}
+
+	for k, v := range pc.Params {
+		config.SetParameter(k, v...)
+	}
+
+	return config
+}
+
+func (pc *PushConfig) String() string {
+
+	return fmt.Sprintf("<pushconfig identities:%s identityfilters:%s>", pc.Identities, pc.IdentityFilters)
+}
---
+++ push_config_test.go
@@ -0,0 +1,518 @@
+// Copyright 2019 Aporeto Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package elemental
+
+import (
+	"net/url"
+	"reflect"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestPushConfig_FilterForIdentity(t *testing.T) {
+
+	tests := map[string]struct {
+		pushConfig     *PushConfig
+		identity       string
+		expectedFilter *Filter
+		expectedFound  bool
+	}{
+		"should return the identity filter for the given identity and true if it exists": {
+			pushConfig: &PushConfig{
+				Identities: map[string][]EventType{
+					"identity_one": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+				},
+				IdentityFilters: map[string]string{
+					"identity_one": "namespace == /liverpool-fc and environment == production",
+				},
+			},
+			identity: "identity_one",
+			expectedFilter: NewFilterComposer().And(
+				NewFilterComposer().WithKey("namespace").Equals("/liverpool-fc").Done(),
+				NewFilterComposer().WithKey("environment").Equals("production").Done(),
+			).Done(),
+			expectedFound: true,
+		},
+		"should return a nil filter and false if the push config has no identity filters configured": {
+			pushConfig: &PushConfig{
+				Identities: map[string][]EventType{
+					"identity_one": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+				},
+			},
+			identity:       "identity_one",
+			expectedFilter: nil,
+			expectedFound:  false,
+		},
+		"should return a nil filter and false if the push config has no identity filter for the provided identity": {
+			pushConfig: &PushConfig{
+				Identities: map[string][]EventType{
+					"identity_one": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+				},
+				IdentityFilters: map[string]string{
+					"identity_one": "namespace == /liverpool-fc and environment == production",
+				},
+			},
+			identity:       "some_other_identity",
+			expectedFilter: nil,
+			expectedFound:  false,
+		},
+	}
+
+	for scenario, testCase := range tests {
+		t.Run(scenario, func(t *testing.T) {
+			if testCase.pushConfig.IdentityFilters != nil && len(testCase.pushConfig.IdentityFilters) > 0 {
+				if err := testCase.pushConfig.ParseIdentityFilters(); err != nil {
+					t.Fatalf("test setup invalid - failed to parse identity filters for the configured push config: %+v", err)
+				}
+			}
+
+			filter, found := testCase.pushConfig.FilterForIdentity(testCase.identity)
+
+			if found != testCase.expectedFound {
+				t.Errorf("expectation failed\n"+
+					"expected identity to be found: %t\n"+
+					"actual: %t\n",
+					testCase.expectedFound,
+					found)
+			}
+
+			if !reflect.DeepEqual(filter, testCase.expectedFilter) {
+				t.Errorf("returned filter does not match expected filter\n"+
+					"expected: %+v\n"+
+					"actual: %+v\n",
+					testCase.expectedFilter,
+					filter)
+			}
+		})
+	}
+}
+
+func TestPushConfig_ParseIdentityFilters(t *testing.T) {
+
+	tests := map[string]struct {
+		pushConfig      *PushConfig
+		expectedFilters map[string]*Filter
+		expectedError   bool
+	}{
+		"should successfully parse the filters and populate the parsed filters attribute of push config": {
+			pushConfig: &PushConfig{
+				Identities: map[string][]EventType{
+					"identity_one": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+					"identity_two": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+				},
+				IdentityFilters: map[string]string{
+					"identity_one": "namespace == /liverpool-fc and environment == production",
+					"identity_two": "namespace == /barcelona-fc and environment == staging",
+				},
+			},
+			expectedFilters: map[string]*Filter{
+				"identity_one": NewFilterComposer().And(
+					NewFilterComposer().WithKey("namespace").Equals("/liverpool-fc").Done(),
+					NewFilterComposer().WithKey("environment").Equals("production").Done(),
+				).Done(),
+				"identity_two": NewFilterComposer().And(
+					NewFilterComposer().WithKey("namespace").Equals("/barcelona-fc").Done(),
+					NewFilterComposer().WithKey("environment").Equals("staging").Done(),
+				).Done(),
+			},
+			expectedError: false,
+		},
+		"should return an error in the event that an identity filter is being defined on an identity that is NOT declared in the identities attribute": {
+			pushConfig: &PushConfig{
+				Identities: map[string][]EventType{},
+				IdentityFilters: map[string]string{
+					"identity_one": "namespace == /liverpool-fc and environment == production",
+				},
+			},
+			expectedFilters: map[string]*Filter{},
+			expectedError:   true,
+		},
+		"should return an error in the event that an identity filter could not be parsed": {
+			pushConfig: &PushConfig{
+				Identities: map[string][]EventType{
+					"identity_one": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+				},
+				IdentityFilters: map[string]string{
+					// notice how this will result in a parsing error due to an invalid comparator "======="
+					"identity_four": "namespace ======= /liverpool-fc",
+				},
+			},
+			expectedFilters: map[string]*Filter{},
+			expectedError:   true,
+		},
+		"should zero the parsed identity filters attribute in the event that an error occurs due to an undeclared identity": {
+			pushConfig: &PushConfig{
+				Identities: map[string][]EventType{
+					"identity_one": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+					"identity_two": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+					"identity_three": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+					"identity_four": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+				},
+				IdentityFilters: map[string]string{
+					// notice how this is an undeclared identity as 'identity_five' is not declared in the PushConfig's
+					// 'Identities' attribute
+					"identity_five": "namespace == /liverpool-fc and environment == production",
+				},
+			},
+			// the push config's parsed identities attribute should be zero'd out
+			expectedFilters: map[string]*Filter{},
+			expectedError:   true,
+		},
+		"should zero the parsed identity filters attribute in the event that an error occurs due to a parsing error": {
+			pushConfig: &PushConfig{
+				Identities: map[string][]EventType{
+					"identity_one": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+					"identity_two": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+					"identity_three": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+					"identity_four": {
+						EventCreate,
+						EventUpdate,
+						EventDelete,
+					},
+				},
+				IdentityFilters: map[string]string{
+					// notice how this will result in a parsing error due to an invalid comparator "======="
+					"identity_four": "namespace ======= /liverpool-fc",
+				},
+			},
+			// the push config's parsed identities attribute should be zero'd out
+			expectedFilters: map[string]*Filter{},
+			expectedError:   true,
+		},
+	}
+
+	for scenario, testCase := range tests {
+		t.Run(scenario, func(t *testing.T) {
+			err := testCase.pushConfig.ParseIdentityFilters()
+
+			if (err != nil) != testCase.expectedError {
+				t.Errorf("\n"+
+					"error expectation failed\n"+
+					"test case expected an error: %t\n"+
+					"an error actually occur: %t\n"+
+					"actual error: %+v\n",
+					testCase.expectedError,
+					err != nil,
+					err,
+				)
+			}
+
+			if !reflect.DeepEqual(testCase.pushConfig.parsedIdentityFilters, testCase.expectedFilters) {
+				t.Errorf("the parsed filters did not match what was expected\n"+
+					"expected: %+v\n"+
+					"actual: %+v\n",
+					testCase.expectedFilters,
+					testCase.pushConfig.parsedIdentityFilters)
+			}
+		})
+	}
+}
+
+func TestPushConfig_NewPushConfig(t *testing.T) {
+
+	Convey("Given I create a new PushConfig", t, func() {
+
+		f := NewPushConfig()
+
+		Convey("Then it should be correctly initialized", func() {
+			So(f.Identities, ShouldNotBeNil)
+			So(f.IdentityFilters, ShouldNotBeNil)
+			So(f.parsedIdentityFilters, ShouldNotBeNil)
+			So(f.Params, ShouldBeNil)
+		})
+	})
+}
+
+// just keeping this for backwards compatibility so we don't break the API by accident by removing the old constructor API
+func TestPushConfig_NewPushFilter(t *testing.T) {
+
+	Convey("Given I create a new NewPushFilter", t, func() {
+
+		f := NewPushFilter()
+
+		Convey("Then it should be correctly initialized", func() {
+			So(f.Identities, ShouldNotBeNil)
+			So(f.IdentityFilters, ShouldNotBeNil)
+			So(f.parsedIdentityFilters, ShouldNotBeNil)
+			So(f.Params, ShouldBeNil)
+		})
+	})
+}
+
+func TestPushConfig_Duplicate(t *testing.T) {
+
+	Convey("Given I create a new PushConfig", t, func() {
+
+		f := NewPushConfig()
+
+		f.SetParameter("key", "values")
+
+		f.FilterIdentity("i1", EventCreate, EventDelete)
+		f.FilterIdentity("i2", EventCreate, EventDelete)
+
+		testFilter := NewFilterComposer().
+			And(
+				NewFilterComposer().
+					WithKey("propertyA").
+					Equals("someValue").
+					Done(),
+				NewFilterComposer().
+					WithKey("propertyB").
+					Equals("someValue").
+					Done(),
+			).Done()
+
+		f.IdentityFilters = map[string]string{
+			"i1": testFilter.String(),
+		}
+
+		// parse the identity filters so the private attribute 'IdentityFilters' gets populated
+		So(f.ParseIdentityFilters(), ShouldBeNil)
+
+		Convey("When I call Duplicate", func() {
+
+			dup := f.Duplicate()
+
+			Convey("Then it should be correctly duplicated", func() {
+				So(dup.Identities, ShouldResemble, f.Identities)
+				So(dup.Identities, ShouldNotEqual, f.Identities)
+
+				So(dup.IdentityFilters, ShouldResemble, f.IdentityFilters)
+				So(dup.IdentityFilters, ShouldNotEqual, f.IdentityFilters)
+
+				So(dup.parsedIdentityFilters, ShouldResemble, f.parsedIdentityFilters)
+				So(dup.parsedIdentityFilters, ShouldNotEqual, f.parsedIdentityFilters)
+
+				So(dup.Params, ShouldResemble, f.Params)
+				So(dup.Params, ShouldNotEqual, f.Params)
+			})
+		})
+	})
+}
+
+func TestPushConfig_Parameters(t *testing.T) {
+
+	Convey("Given I create a new PushConfig", t, func() {
+
+		f := NewPushConfig()
+
+		Convey("When I call SetParameter", func() {
+
+			f.SetParameter("key1", "v1", "v2")
+			f.SetParameter("key2", "v3")
+
+			Convey("Then the parameter should be set", func() {
+
+				So(f.Parameters(), ShouldResemble, url.Values{
+					"key1": []string{"v1", "v2"},
+					"key2": []string{"v3"},
+				})
+
+				So(f.Parameters(), ShouldNotEqual, f.Params)
+			})
+		})
+	})
+
+	Convey("Given I have a push filter with no parameters", t, func() {
+
+		f := NewPushConfig()
+
+		Convey("When I call Parameters", func() {
+
+			p := f.Parameters()
+
+			Convey("Then p should be nil", func() {
+				So(p, ShouldBeNil)
+			})
+		})
+	})
+}
+
+func TestPushConfig_IsFilteredOut(t *testing.T) {
+
+	Convey("Given I create a new PushConfig", t, func() {
+
+		f := NewPushConfig()
+
+		Convey("When I check if i1 is filtered with a nil value for identities", func() {
+
+			f.Identities = nil
+
+			filtered1 := f.IsFilteredOut("i1", EventDelete)
+			filtered2 := f.IsFilteredOut("i2", EventDelete)
+
+			Convey("Then filtered1 should be false", func() {
+				So(filtered1, ShouldBeFalse)
+			})
+
+			Convey("Then filtered2 should be false", func() {
+				So(filtered2, ShouldBeFalse)
+			})
+		})
+
+		Convey("When I check if i1 is filtered with an empty identities list", func() {
+
+			filtered1 := f.IsFilteredOut("i1", EventDelete)
+			filtered2 := f.IsFilteredOut("i2", EventDelete)
+
+			Convey("Then filtered1 should be false", func() {
+				So(filtered1, ShouldBeFalse)
+			})
+
+			Convey("Then filtered2 should be false", func() {
+				So(filtered2, ShouldBeFalse)
+			})
+		})
+
+		Convey("When I check if i1 is filtered", func() {
+
+			f.FilterIdentity("i1")
+
+			filtered1 := f.IsFilteredOut("i1", EventDelete)
+			filtered2 := f.IsFilteredOut("i2", EventDelete)
+
+			Convey("Then filtered1 should be false", func() {
+				So(filtered1, ShouldBeFalse)
+			})
+
+			Convey("Then filtered2 should be false", func() {
+				So(filtered2, ShouldBeTrue)
+			})
+		})
+
+		Convey("When I add a filter for i1 on Create and Delete", func() {
+
+			f.FilterIdentity("i1", EventCreate, EventDelete)
+			f.FilterIdentity("i2")
+
+			Convey("Then create and delete should not be filtered out on i1", func() {
+				So(f.IsFilteredOut("i1", EventCreate), ShouldBeFalse)
+				So(f.IsFilteredOut("i1", EventDelete), ShouldBeFalse)
+			})
+
+			Convey("Then update should be filtered out on i1", func() {
+				So(f.IsFilteredOut("i1", EventUpdate), ShouldBeTrue)
+			})
+
+			Convey("Then nothing should be filtered out on i2", func() {
+				So(f.IsFilteredOut("i2", EventCreate), ShouldBeFalse)
+				So(f.IsFilteredOut("i2", EventUpdate), ShouldBeFalse)
+				So(f.IsFilteredOut("i2", EventDelete), ShouldBeFalse)
+			})
+		})
+
+		Convey("When I add a filter for i1 on nothing", func() {
+
+			f.FilterIdentity("i1")
+			f.FilterIdentity("i2")
+
+			Convey("Then everything should not be filtered out on i1", func() {
+				So(f.IsFilteredOut("i1", EventCreate), ShouldBeFalse)
+				So(f.IsFilteredOut("i1", EventDelete), ShouldBeFalse)
+				So(f.IsFilteredOut("i1", EventUpdate), ShouldBeFalse)
+			})
+
+			Convey("Then nothing should be filtered out on i2", func() {
+				So(f.IsFilteredOut("i2", EventCreate), ShouldBeFalse)
+				So(f.IsFilteredOut("i2", EventUpdate), ShouldBeFalse)
+				So(f.IsFilteredOut("i2", EventDelete), ShouldBeFalse)
+			})
+		})
+	})
+}
+
+func TestPushConfig_String(t *testing.T) {
+
+	Convey("Given I create a new PushConfig", t, func() {
+
+		f := NewPushConfig()
+
+		f.FilterIdentity("i1", EventCreate, EventDelete)
+
+		testFilter := NewFilterComposer().
+			And(
+				NewFilterComposer().
+					WithKey("propertyA").
+					Equals("someValue").
+					Done(),
+				NewFilterComposer().
+					WithKey("propertyB").
+					Equals("someValue").
+					Done(),
+			).Done()
+
+		f.IdentityFilters = map[string]string{
+			"i1": testFilter.String(),
+		}
+
+		Convey("When I call the String Method", func() {
+			s := f.String()
+
+			Convey("Then it should be correctly printed", func() {
+				So(s, ShouldEqual, `<pushconfig identities:map[i1:[create delete]] identityfilters:map[i1:((propertyA == "someValue") and (propertyB == "someValue"))]>`)
+			})
+		})
+	})
+}
