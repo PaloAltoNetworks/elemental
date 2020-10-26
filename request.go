5ce6ed45125bcf6de99d3397107ed94018f63b5c
CyrilPeponnet <cyril@peponnet.fr>
2020-10-26 14:58:42 -0700

    fix/request: Clean up result of split first

--- request.go
+++ request.go
@@ -276,8 +276,8 @@ func NewRequestFromHTTPRequest(req *http.Request, manager ModelManager) (*Reques
 	var clientIP string
 	// Parse the Forwarded header as they can be in form
 	// X-Forwarded-For: <original_client>, [proxy1], [proxy2>
-	if ip := strings.Split(req.Header.Get("X-Forwarded-For"), ",")[0]; ip != "" {
-		clientIP = strings.TrimSpace(ip)
+	if ip := strings.TrimSpace(strings.Split(req.Header.Get("X-Forwarded-For"), ",")[0]); ip != "" {
+		clientIP = ip
 	} else if ip := req.Header.Get("X-Real-IP"); ip != "" {
 		clientIP = ip
 	} else {
