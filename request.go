8ad73fb22718e45cc4b6311fe0a28681e029548e
Cyril Peponnet <cyril@aporeto.com>
2020-10-27 10:37:36 -0700

    fix/request: Use first part of X-Forwarded-For as client IP. (#82)
    * fix/request: Use first part of X-Forwarded-For as client IP.
    
    * fix/request: Clean up result of split first

--- request.go
+++ request.go
@@ -274,7 +274,9 @@ func NewRequestFromHTTPRequest(req *http.Request, manager ModelManager) (*Reques
 	}
 
 	var clientIP string
-	if ip := req.Header.Get("X-Forwarded-For"); ip != "" {
+	// Parse the Forwarded header as they can be in form
+	// X-Forwarded-For: <original_client>, [proxy1], [proxy2>
+	if ip := strings.TrimSpace(strings.Split(req.Header.Get("X-Forwarded-For"), ",")[0]); ip != "" {
 		clientIP = ip
 	} else if ip := req.Header.Get("X-Real-IP"); ip != "" {
 		clientIP = ip
