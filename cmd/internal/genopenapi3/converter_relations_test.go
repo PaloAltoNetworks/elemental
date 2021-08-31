da48a53b1ae37d769ccda71307d59c5fe457e515
Mohammad Alkhamis <malkhamis@paloaltonetworks.com>
2021-08-31 15:20:01 -0700

    new: test case for root model being ignored

--- cmd/internal/genopenapi3/converter_relations_test.go
+++ cmd/internal/genopenapi3/converter_relations_test.go
@@ -1,7 +1,46 @@
 package genopenapi3
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestConverter_Do__relations_root(t *testing.T) {
+
+	cases := map[string]testCase{
+		//
+		"root-model-should-be-ignored": {
+			inSpec: `
+				model:
+					root: true
+					rest_name: root
+					resource_name: root
+					entity_name: Root
+					package: root
+					group: core
+					description: root object.
+			`,
+			outDoc: `
+				{
+					"components": {},
+					"paths": {}
+				}
+			`,
+		},
+	}
+
+	rootTmpDir, err := os.MkdirTemp("", t.Name()+"_*")
+	if err != nil {
+		t.Fatalf("error creating temporary directory for test function: %v", err)
+	}
+	t.Cleanup(func() { os.RemoveAll(rootTmpDir) })
+
+	tcRunner := &testCaseRunner{
+		t:          t,
+		rootTmpDir: rootTmpDir,
+	}
+	for name, testCase := range cases {
+		tcRunner.Run(name, testCase)
+	}
 
 }
