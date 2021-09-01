ae6ab4ca3eb5ee0e254a8db7f70a40d57ece9ceb
Mohammad Alkhamis <malkhamis@paloaltonetworks.com>
2021-09-01 15:09:03 -0700

    new: test case to ensure paramID isn't duplicated w/ multiple relations

--- cmd/internal/genopenapi3/converter_relations_model_nonroot_test.go
+++ cmd/internal/genopenapi3/converter_relations_model_nonroot_test.go
@@ -175,6 +175,103 @@ func TestConverter_Do__modelRelations_nonRoot(t *testing.T) {
 				}
 			`,
 		},
+
+		"get-put-delete-by-ID--do-not-duplicate-param-ID": {
+			inSpec: `
+				model:
+					rest_name: resource
+					resource_name: resources
+					entity_name: Resource
+					package: None
+					group: N/A
+					description: useful description.
+					get:
+						description: Retrieves the resource with the given ID.
+					delete:
+						description: Deletes the resource with the given ID.
+					update:
+						description: Updates the resource with the given ID.
+			`,
+			outDoc: `
+				{
+					"components": {
+						"schemas": {
+							"resource": {
+								"type": "object"
+							}
+						}
+					},
+					"paths": {
+						"/resources/{id}": {
+							"parameters": [
+								{
+									"in": "path",
+									"name": "id",
+									"required": true,
+									"schema": {
+										"type": "string"
+									}
+								}
+							],
+							"get": {
+								"description": "Retrieves the resource with the given ID.",
+								"responses": {
+									"200": {
+										"description": "n/a",
+										"content": {
+											"application/json": {
+												"schema": {
+													"$ref": "#/components/schemas/resource"
+												}
+											}
+										}
+									}
+								}
+							},
+							"delete": {
+								"description": "Deletes the resource with the given ID.",
+								"responses": {
+									"200": {
+										"description": "n/a",
+										"content": {
+											"application/json": {
+												"schema": {
+													"$ref": "#/components/schemas/resource"
+												}
+											}
+										}
+									}
+								}
+							},
+							"put": {
+								"description": "Updates the resource with the given ID.",
+								"requestBody": {
+									"content": {
+										"application/json": {
+											"schema": {
+												"$ref": "#/components/schemas/resource"
+											}
+										}
+									}
+								},
+								"responses": {
+									"200": {
+										"description": "n/a",
+										"content": {
+											"application/json": {
+												"schema": {
+													"$ref": "#/components/schemas/resource"
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			`,
+		},
 	}
 	runAllTestCases(t, cases)
 }
