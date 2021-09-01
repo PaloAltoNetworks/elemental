35d493aef56cd78b76a0246091adee783c56929e
Mohammad Alkhamis <malkhamis@paloaltonetworks.com>
2021-09-01 10:16:15 -0700

    new: test case for model relation action deleteByID

--- cmd/internal/genopenapi3/converter_relations_nonroot_spec_test.go
+++ cmd/internal/genopenapi3/converter_relations_nonroot_spec_test.go
@@ -4,6 +4,8 @@ import "testing"
 
 func TestConverter_Do__relations_nonroot_spec(t *testing.T) {
 	cases := map[string]testCase{
+
+		//
 		"model-get-by-ID": {
 			inSpec: `
 				model:
@@ -57,6 +59,62 @@ func TestConverter_Do__relations_nonroot_spec(t *testing.T) {
 				}
 			`,
 		},
+
+		//
+		"model-delete-by-ID": {
+			inSpec: `
+				model:
+					rest_name: resource
+					resource_name: resources
+					entity_name: Resource
+					package: None
+					group: N/A
+					description: useful description.
+					delete:
+						description: Deletes the resource with the given ID.
+			`,
+			outDoc: `
+				{
+					"components": {
+						"schemas": {
+							"resource": {
+								"type": "object"
+							}
+						}
+					},
+					"paths": {
+						"/resources/{id}": {
+							"parameters": [
+								{
+									"in": "path",
+									"name": "id",
+									"required": true,
+									"schema": {
+										"type": "string"
+									}
+								}
+							],
+							"delete": {
+								"description": "Deletes the resource with the given ID.",
+								"responses": {
+									"200": {
+										"description": "n/a",
+										"content": {
+											"application/json": {
+												"schema": {
+													"$ref": "#/components/schemas/resource"
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			`,
+		},
+
 	}
 	runAllTestCases(t, cases)
 }
