d250e7d618fbdc6911a08cf0e8aae2b93d70343e
Mohammad Alkhamis <malkhamis@paloaltonetworks.com>
2021-09-01 10:11:15 -0700

    new: test case for model relation action getByID

--- cmd/internal/genopenapi3/converter_relations_nonroot_spec_test.go
+++ cmd/internal/genopenapi3/converter_relations_nonroot_spec_test.go
@@ -3,6 +3,60 @@ package genopenapi3
 import "testing"
 
 func TestConverter_Do__relations_nonroot_spec(t *testing.T) {
-	cases := map[string]testCase{}
+	cases := map[string]testCase{
+		"model-get-by-ID": {
+			inSpec: `
+				model:
+					rest_name: resource
+					resource_name: resources
+					entity_name: Resource
+					package: None
+					group: N/A
+					description: useful description.
+					get:
+						description: Retrieves the resource with the given ID.
+			`,
+			outDoc: `
+				{
+					"components": {
+						"schemas": {
+							"resource": {
+								"type": "object"
+							}
+						}
+					},
+					"paths": {
+						"/resources/{id}": {
+							"parameters": [
+								{
+									"in": "path",
+									"name": "id",
+									"required": true,
+									"schema": {
+										"type": "string"
+									}
+								}
+							],
+							"get": {
+								"description": "Retrieves the resource with the given ID.",
+								"responses": {
+									"200": {
+										"description": "n/a",
+										"content": {
+											"application/json": {
+												"schema": {
+													"$ref": "#/components/schemas/resource"
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			`,
+		},
+	}
 	runAllTestCases(t, cases)
 }
