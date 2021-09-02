7c13d4d2585f7c0d11d8df48d35ec442ff485bbe
Mohammad Alkhamis <malkhamis@paloaltonetworks.com>
2021-09-01 17:11:22 -0700

    new: empty test file with a skipped test

---
+++ cmd/internal/genopenapi3/exports_test.go
@@ -0,0 +1,7 @@
+package genopenapi3
+
+import "testing"
+
+func TestGeneratorFunc(t *testing.T) {
+	t.Skip("TODO--The GeneratorFunc adapter is likely to change; so let's spare testing for later")
+}
